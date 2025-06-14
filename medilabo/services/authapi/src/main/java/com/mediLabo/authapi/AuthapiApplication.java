4be31852fa66852fe46184ac40a84d9710e6728b
podisto <omardione1991@gmail.com>
2025-06-14 12:33:51 -0400

    setup users and postgres

--- medilabo/services/authapi/src/main/java/com/mediLabo/authapi/AuthapiApplication.java
+++ medilabo/services/authapi/src/main/java/com/mediLabo/authapi/AuthapiApplication.java
@@ -2,12 +2,21 @@ package com.mediLabo.authapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AuthapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthapiApplication.class, args);
+	}
+
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
