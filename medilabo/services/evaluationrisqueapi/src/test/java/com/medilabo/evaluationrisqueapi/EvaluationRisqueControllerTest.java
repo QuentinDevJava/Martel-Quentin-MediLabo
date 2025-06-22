7d2ba0f16f2d34193211048d827f98b7f4de8cd5
QuentinDevJava <168752018+QuentinDevJava@users.noreply.github.com>
2025-06-22 18:02:28 +0200

    Update evaluationrisqueapi test

---
+++ medilabo/services/evaluationrisqueapi/src/test/java/com/medilabo/evaluationrisqueapi/EvaluationRisqueControllerTest.java
@@ -0,0 +1,41 @@
+package com.medilabo.evaluationrisqueapi;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.medilabo.evaluationrisqueapi.enums.DangerLevel;
+import com.medilabo.evaluationrisqueapi.service.EvaluationRisqueService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class EvaluationRisqueControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private EvaluationRisqueService evaluationRisqueService;
+
+    @Test
+    void getDiabetesReportWithpatientId_ShouldReturnDangerLevel() throws Exception {
+
+	int patientId = 1;
+	DangerLevel expectedLevel = DangerLevel.IN_DANGER;
+
+	when(evaluationRisqueService.generateDiabetesReport(patientId)).thenReturn(expectedLevel);
+
+	mockMvc.perform(get("/api/evaluationrisque/patients/{patientId}", patientId)).andExpect(status().isOk())
+		.andExpect(content().json("\"IN_DANGER\""));
+    }
+}
