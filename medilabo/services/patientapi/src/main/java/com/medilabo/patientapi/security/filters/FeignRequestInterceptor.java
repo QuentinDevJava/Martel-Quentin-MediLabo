54e8255c5f3f6fa92e4582dfa06de0e58ed89592
QuentinDevJava <168752018+QuentinDevJava@users.noreply.github.com>
2025-07-02 00:21:24 +0200

    Add FeignRequestInterceptor to patient api

---
+++ medilabo/services/patientapi/src/main/java/com/medilabo/patientapi/security/filters/FeignRequestInterceptor.java
@@ -0,0 +1,32 @@
+package com.medilabo.patientapi.security.filters;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Component
+@Slf4j
+public class FeignRequestInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        log.info("Adding authorization token to header");
+        String token = getTokenFromRequest();
+        if (token != null) {
+            requestTemplate.header(HttpHeaders.AUTHORIZATION, token);
+        }
+    }
+
+    private String getTokenFromRequest() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) {
+            return null;
+        }
+
+        return attributes.getRequest().getHeader(HttpHeaders.AUTHORIZATION);
+    }
+}
